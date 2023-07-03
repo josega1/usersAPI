899e9506b06abc2e4b01c337e4067d9e47dfdae9
unknown <jr747524@gmail.com>
2023-07-03 09:26:21 -0500

    userAPI

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,47 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+
+const getAll = catchError(async(req, res) => {
+    // Operaciones...
+    const users = await User.findAll();
+    return res.json(users)
+});
+
+const create = catchError(async(req, res) => {
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const users = await User.create({
+        first_name, 
+        last_name, 
+        email, 
+        password, 
+        birthday
+    })
+    return res.status(201).json(users);
+});
+
+const getOne = catchError(async(req, res) => {
+    const{ id } = req.params;
+    const users = await User.findByPk(id); 
+    return res.json(users);
+ });
+
+const remove = catchError(async(req, res) => {
+    const { id } = req.params;
+    await User.destroy({ where: {id: id} })
+    return res.sendStatus(204);
+});
+
+const update = catchError(async(req, res) =>{
+    const { id } = req.params;
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const users = await User.update({first_name, last_name, email, password, birthday}, { where: {id}, returning: {id} })
+    return res.json(users);
+});
+
+module.exports = {
+    getAll,
+    create,
+    getOne,
+    remove,
+    update
+}
